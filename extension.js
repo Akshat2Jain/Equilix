41cb1d59a73f493afca23542b6fbb70ebed8c8e0
akshat2jain <akshat.2024it1031@kiet.edu>
2023-07-18 18:15:20 +0530

    added features

--- extension.js
+++ extension.js
@@ -6,7 +6,8 @@ function activate(context) {
   const disposable = vscode.workspace.onWillSaveTextDocument((event) => {
     const document = event.document;
     const text = document.getText();
-    const convertedText = convertEquals(text);
+    const unusedVariables = detectUnusedVariables(text);
+    const convertedText = convertEquals(text, unusedVariables);
     if (text !== convertedText) {
       const edit = new vscode.TextEdit(
         new vscode.Range(
@@ -21,17 +22,64 @@ function activate(context) {
       vscode.window.showInformationMessage(
         `Equilix fixed line no: ${lineNumbers.join(", ")}`
       );
-      5;
+      if (unusedVariables.length > 0) {
+        vscode.window.showWarningMessage(
+          `Detected ${
+            unusedVariables.length
+          } unused variables: ${unusedVariables.join(", ")}.`
+        );
+      }
     }
   });
 
   context.subscriptions.push(disposable);
 }
 
-function convertEquals(text) {
-  const convertedText = text.replace(/ == /g, " === ");
-  return convertedText.replace(/ != /g, " !== ");
+function detectUnusedVariables(text) {
+  // Regular expression to detect variable declarations and usages
+  const variableRegex = /\b(?:let|const|var)\s+([A-Za-z_]\w*)\b/g;
+
+  const declaredVariables = new Set();
+  const usedVariables = new Set();
+
+  // Find all declared variables
+  text.replace(variableRegex, (_, variableName) => {
+    declaredVariables.add(variableName);
+    return "";
+  });
+
+  // Find all used variables
+  text.replace(/([A-Za-z_]\w*)/g, (_, variableName) => {
+    usedVariables.add(variableName);
+    return "";
+  });
+
+  // Filter out the unused variables
+  return [...declaredVariables].filter(
+    (variable) => !usedVariables.has(variable)
+  );
 }
+
+function convertForLoops(text) {
+  // Convert 'for' loops to 'forEach' or 'map' methods
+  const convertedText = text.replace(
+    /for\s*\(\s*(?:let|var|const)?\s*(\w+)\s*=\s*(\w+)\s*;\s*(\1\s*(<|<=|>|>=|===|!==)\s*\w+)\s*;\s*(\1\s*(\+\+|--|\+=|-=|\*=|\/=|%=)\s*)\)/g,
+    "($2).forEach(($1) =>"
+  );
+  return convertedText;
+}
+
+function convertEquals(text) {
+  // Convert 'var' to 'const'
+  const constConvertedText = text.replace(/\bvar\b/g, "const");
+
+  const forLoopsConvertedText = convertForLoops(constConvertedText);
+
+  // Convert equality and inequality operators
+  const convertedEquals = forLoopsConvertedText.replace(/ == /g, " === ");
+  return convertedEquals.replace(/ != /g, " !== ");
+}
+
 function getConvertedLineNumbers(originalText, convertedText) {
   const originalLines = originalText.split("\n");
   const convertedLines = convertedText.split("\n");
